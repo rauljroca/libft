7d8cb9f183a5a54cdcfe2fdd81aabdcd51a34a9e
Raul j Roca godia <rroca-go@c1r10s2.42madrid.com>
2022-08-24 12:01:48 +0200

    ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: rroca-go <rroca-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 01:17:39 by rroca-go@st       #+#    #+#             */
-/*   Updated: 2022/08/24 11:40:19 by rroca-go         ###   ########.fr       */
+/*   Updated: 2022/08/24 12:01:02 by rroca-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ t_list	*ft_lstnew(void *content)
 	newlist->next = NULL;
 	return (newlist);
 }
-
+/*
 #include <unistd.h>
 #include <stdio.h>
 int	main(void)
@@ -38,7 +38,7 @@ int	main(void)
 	printf("Tamaño = %zu \n", sizeof(result->content));
 	return 0;
 }
-/*
+
 Parámetros content:
 el contenido con el que crear el nodo.
 Valor devuelto
